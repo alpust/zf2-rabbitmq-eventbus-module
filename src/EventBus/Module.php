2b1925a841c896792b52b731067dd5f811bb02ac
Paul <paul@paulpc.paulpc>
2015-04-13 20:29:06 +0300

    small cleanup

--- src/EventBus/Module.php
+++ src/EventBus/Module.php
@@ -3,4 +3,4 @@
  * This file is placed here for compatibility with Zendframework 2's ModuleManager.
  * It allows usage of this module even without composer.
  */
-require_once __DIR__ . '/EventBus/PortAdapter/ZF2/Module.php';
+require_once __DIR__ . '/PortAdapter/ZF2/Module.php';
